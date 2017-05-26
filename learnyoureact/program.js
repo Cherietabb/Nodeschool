88ff472fb848e0996f8b4f1d97aa8933c644cfc7
Cherie Tabb <ctabb@mcmail.cc>
2017-05-26 08:38:25 -0500

    completed another learnyoureact challenge

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -1,5 +1,49 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var script = DOM.script;
 var express = require('express');
 var app = express();
+
+var browserify = require('browserify');
+var babelify = require('babelify');
+
+require('babel/register')({
+    ignore: false
+});
+
+app.use('/bundle.js', function (req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({ debug: true })
+        .transform(babelify.configure({
+            presets: ["react", "es2015"],
+            compact: false
+        }))
+        .require("./app.js", { entry: true })
+        .bundle()
+        .pipe(res);
+});
+
+app.use('/', function (req, res) {
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
+});
 
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
