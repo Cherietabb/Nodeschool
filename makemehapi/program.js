51001ca77ee3de210c8e4ba8799d8820419de92c
Cherie Tabb <cherietabb@gmail.com>
2017-07-17 11:33:53 -0500

    initial commits for new lessons

---
+++ makemehapi/program.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.route({path: '/home', method: 'GET', handler: (request, reply) => {
+  reply('Hello world')
+}});
+
+server.start(function() {
+  console.log('app running at: ', server.info.url);
+})
